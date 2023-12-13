f34fe02d0d05e630ce98a13a55e4e37ec829988b
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-13 06:50:56 +0000

    chore: :passport_control: Auth
    - Add Clerk.js authentication.
    - Create authorization and authentication routes.
    - Implement initial setup for authentication.
    - Update page layout.
    - Add avatar png and svg.
    
    Authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
-  return <main>Hello World</main>;
+export default function RootPage() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1 className="text-3xl font-bold">Welcome to Root Page</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
