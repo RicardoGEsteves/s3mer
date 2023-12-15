7d21c9126a9d78ee4236d80c495d4585b62f7da8
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-15 18:01:17 +0000

    feat: :sparkles: Stream and chat features.
    - Create stream functionality.
    - Implement chat feature.
    - Create chat page.
    - Create stream service.
    - Create stream actions.
    - Create loading skeletons for chat page.
    
    Users can now define their chat settings.

---
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -0,0 +1,40 @@
+import { getSelf } from "@/lib/auth-service";
+import { getStreamByUserId } from "@/lib/stream-service";
+
+import ToggleCard from "./_components/toggle-card";
+
+const ChatPage = async () => {
+  const self = await getSelf();
+  const stream = await getStreamByUserId(self.id);
+
+  if (!stream) {
+    throw new Error("Stream not found");
+  }
+
+  return (
+    <div className="p-6">
+      <div className="mb-4">
+        <h1 className="text-2xl font-semibold">Settings</h1>
+      </div>
+      <div className="space-y-4">
+        <ToggleCard
+          field="isChatEnabled"
+          label="Enable chat"
+          value={stream.isChatEnabled}
+        />
+        <ToggleCard
+          field="isChatDelayed"
+          label="Delay chat"
+          value={stream.isChatDelayed}
+        />
+        <ToggleCard
+          field="isChatFollowersOnly"
+          label="Must be following to chat"
+          value={stream.isChatFollowersOnly}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
