028abec18f273ba2b8d87cd96baa52f3b938bf21
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-12-17 21:53:22 +0000

    feat: :sparkles: Search feature.
    - Implement search feature.
    - Create search page.
    - Create search-service.ts.
    - Implement results and card result.
    
    Users can now search for their streamer.

---
+++ app/(browse)/search/_components/results.tsx
@@ -0,0 +1,48 @@
+import { getSearch } from "@/lib/search-service";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import ResultCard, { ResultCardSkeleton } from "./result-card";
+
+interface ResultsProps {
+  term?: string;
+}
+
+const Results = async ({ term }: ResultsProps) => {
+  const data = await getSearch(term);
+
+  return (
+    <div>
+      <h2 className="text-lg font-semibold mb-4">
+        Results for &quot;{term}&quot;
+      </h2>
+      {data.length === 0 && (
+        <p className="text-muted-foreground text-sm">
+          No results found. Try searching for someone else.
+        </p>
+      )}
+      <div className="flex flex-col gap-y-4">
+        {data.map((result) => (
+          <ResultCard
+            data={result}
+            key={result.id}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Results;
+
+export const ResultsSkeleton = () => {
+  return (
+    <div>
+      <Skeleton className="h-8 w-[290px] mb-4" />
+      <div className="flex flex-col gap-y-4">
+        {[...Array(4)].map((_, i) => (
+          <ResultCardSkeleton key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
